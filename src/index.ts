a11483d04f963b5231bb596d1506785b44c3281e
Raju Ahmed <rajucse1705@gmail.com>
2022-02-24 13:29:37 +0600

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 //errors
-export * from "../src/errors/auth.error";
-export * from "../src/errors/bad-request.error";
-export * from "../src/errors/custom.error";
-export * from "../src/errors/not-found.error";
-export * from "../src/errors/request-validation.error";
+export * from "./errors/auth.error";
+export * from "./errors/bad-request.error";
+export * from "./errors/custom.error";
+export * from "./errors/not-found.error";
+export * from "./errors/request-validation.error";
 
 //interfaces
-export * from "../src/interfaces/pagination.interface";
-export * from "../src/interfaces/sql-builder.interface";
+export * from "./interfaces/pagination.interface";
+export * from "./interfaces/sql-builder.interface";
 
 //utils
-export * from "../src/utils/build-sql.utils";
-export * from "../src/utils/is-json.utils";
-export * from "../src/utils/json-path.utils";
-export * from "../src/utils/matchKey.utils";
-export * from "../src/utils/multer-upload.utils";
-export * from "../src/utils/pagination.utils";
-export * from "../src/utils/response-bangla.utils";
-export * from "../src/utils/service.utils";
-export * from "../src/utils/sql-builder.util";
-export * from "../src/utils/toCamelCase.utils";
+export * from "./utils/build-sql.utils";
+export * from "./utils/is-json.utils";
+export * from "./utils/json-path.utils";
+export * from "./utils/matchKey.utils";
+export * from "./utils/multer-upload.utils";
+export * from "./utils/pagination.utils";
+export * from "./utils/response-bangla.utils";
+export * from "./utils/service.utils";
+export * from "./utils/sql-builder.util";
+export * from "./utils/toCamelCase.utils";
